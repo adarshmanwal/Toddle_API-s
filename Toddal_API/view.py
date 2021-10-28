ab01fd31e1cf64bdd20886df7dfe13dc9f17e6f3
Manwal <manwal@Manwals-MacBook-Pro.local>
2021-10-28 17:17:01 +0530

    Updated first page:

--- Toddal_API/view.py
+++ Toddal_API/view.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse('''
+    <h1>Toddle Backend Task (Virtual Classroom)</h1>
+    <p>Simple stateless microservice with following API endpoints.</p>
+
+    <p>Github link: <a href="https://github.com/adarshmanwal/Toddle_API-s" target="_blank">https://github.com/adarshmanwal/Toddle_API-s</a></p>
+    <p>Documentation: <a href="https://github.com/adarshmanwal/Toddle_API-s/blob/master/README.md" target="_blank">https://github.com/adarshmanwal/Toddle_API-s/blob/master/README.md</a></p>
+    ''')
